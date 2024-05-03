0fdd5a40c3c095cdd5ed50e95c0f2e26afc01ec6
Maksim Salchak <maksim.salchak@rubius.com>
2024-05-03 17:33:21 +0700

    added lock panel

---
+++ src/common/LockPanel/LockPanel.tsx
@@ -0,0 +1,114 @@
+import React, { useRef } from "react";
+import style from "./LockPanel.module.scss";
+import clsx from "clsx";
+
+interface IProps {
+  unlockPage: (value: boolean) => void;
+}
+
+const LockPanel = (props: IProps) => {
+  const { unlockPage } = props;
+
+  const dragBtnRef = useRef<HTMLDivElement | null>(null);
+  const dropZoneRef = useRef<HTMLDivElement | null>(null);
+  const dragRef = useRef<HTMLDivElement | null>(null);
+
+  const handleStart = (
+    e:
+      | React.MouseEvent<HTMLDivElement, MouseEvent>
+      | React.TouchEvent<HTMLDivElement>
+  ) => {
+    e.stopPropagation();
+    const dragBtn = dragBtnRef.current;
+    const drag = dragRef.current;
+
+    if (dragBtn && drag) {
+      dragBtn.classList.add(style.dragging);
+
+      if (e.type === "touchstart") {
+        dragBtn.style.transform = `translate3d(0px, 0px, 0px)`;
+        document.addEventListener("touchmove", handleMove);
+        document.addEventListener("touchend", handleEnd);
+      } else {
+        dragBtn.style.transform = `translate3d(0px, 0px, 0px)`;
+        document.addEventListener("mousemove", handleMove);
+        document.addEventListener("mouseup", handleEnd);
+      }
+    }
+  };
+
+  const handleMove = (e: TouchEvent | MouseEvent) => {
+    const dragBtn = dragBtnRef.current;
+
+    const dragRect = dragRef.current?.getBoundingClientRect();
+
+    if (dragBtn) {
+      let clientX;
+      if (e.type === "touchmove") {
+        clientX = (e as TouchEvent).touches[0].clientX;
+      } else {
+        clientX = (e as MouseEvent).clientX;
+      }
+
+      const offsetX = clientX - dragRect!.left;
+      dragBtn.style.transform = `translate3d(${offsetX - 10}px, 0px, 0px)`;
+
+      if (offsetX > 224) {
+        dragBtn.style.transform = `translate3d(224px, 0px, 0px)`;
+      }
+      if (offsetX < -10) {
+        dragBtn.style.transform = `translate3d(-10px, 0px, 0px)`;
+      }
+    }
+  };
+
+  const handleEnd = () => {
+    const dragBtn = dragBtnRef.current;
+    const dropZone = dropZoneRef.current;
+    if (dragBtn && dropZone) {
+      dragBtn.classList.remove(style.dragging);
+      document.removeEventListener("touchmove", handleMove);
+      document.removeEventListener("touchend", handleEnd);
+      document.removeEventListener("mousemove", handleMove);
+      document.removeEventListener("mouseup", handleEnd);
+
+      const dragRect = dragBtn.getBoundingClientRect();
+      const dropRect = dropZone.getBoundingClientRect();
+
+      if (
+        dragRect.left <= dropRect.right &&
+        dragRect.right >= dropRect.left &&
+        dragRect.top <= dropRect.bottom &&
+        dragRect.bottom >= dropRect.top
+      ) {
+        dragBtn.style.transform = `translate3d(224px, 0px, 0px)`;
+        unlockPage(false);
+      } else {
+        dragBtn.style.transform = "";
+      }
+    }
+  };
+
+  return (
+    <div className={style.lockPanel}>
+      <div className={style.content}>
+        <div className={style.title}>wedding day</div>
+        <div className={style.name}>Жахонгир и Ляззат</div>
+        <div className={style.unlockMessage}>Разблокируйте приглашение:</div>
+
+        <div className={clsx(style.drag)} ref={dragRef}>
+          <div
+            ref={dragBtnRef}
+            className={style.dragButton}
+            onMouseDown={handleStart}
+            onTouchStart={handleStart}
+          ></div>
+          <div className={style.dragLine}></div>
+          <div ref={dropZoneRef} className={style.dropZone}></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LockPanel;
